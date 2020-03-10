c5597934a97550a8b11c2ab85a11629c5efb4911
GitTask <gittask@notused.com>
2020-03-09 21:15:12 -0700

    code cleanup

--- src/KayMcCormick.Dev/ConfigTest/Program.cs
+++ src/KayMcCormick.Dev/ConfigTest/Program.cs
@@ -1,55 +1,55 @@
-﻿using System;
-using KayMcCormick.Dev ;
-using KayMcCormick.Dev.Logging ;
-using KayMcCormick.Dev.ServiceReference1 ;
-using NLog ;
-using AppInstanceInfoRequest = KayMcCormick.Dev.ServiceReference1.AppInstanceInfoRequest ;
+﻿using KayMcCormick.Dev;
+using KayMcCormick.Dev.Logging;
+using KayMcCormick.Dev.ServiceReference1;
+using NLog;
+using System;
+using AppInstanceInfoRequest = KayMcCormick.Dev.ServiceReference1.AppInstanceInfoRequest;
 
 namespace ConfigTest
 {
     static class Program
     {
-        private static readonly Logger Logger = LogManager.GetCurrentClassLogger ( ) ;
-        static void Main ( )
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+        static void Main()
         {
-            AppLoggingConfigHelper.EnsureLoggingConfigured (
+            AppLoggingConfigHelper.EnsureLoggingConfigured(
                                                             Console.WriteLine
-                                                           ) ;
-            Utils.PerformLogConfigDump ( Console.Out ) ;
-            Logger.Info ( "{test}" , new Test1 ( ) { Test2 = new Test2 ( ) { Hello = "derp" } } ) ;
-            LogManager.GetCurrentClassLogger ( ).Info ( "Test log message" ) ;
-            using ( AppInfoServiceClient client = new AppInfoServiceClient ( ) )
+                                                           );
+            Utils.PerformLogConfigDump(Console.Out);
+            Logger.Info("{test}", new Test1() { Test2 = new Test2() { Hello = "derp" } });
+            LogManager.GetCurrentClassLogger().Info("Test log message");
+            using (AppInfoServiceClient client = new AppInfoServiceClient())
             {
                 var appInstanceInfoResponse =
-                    client.GetAppInstanceInfo ( new AppInstanceInfoRequest ( ) ) ;
-                var info = appInstanceInfoResponse.Info ;
-                Console.WriteLine ( info.StartupTime ) ;
+                    client.GetAppInstanceInfo(new AppInstanceInfoRequest());
+                var info = appInstanceInfoResponse.Info;
+                Console.WriteLine(info.StartupTime);
 
-                foreach ( var infoLoggerInfo in info.LoggerInfos )
+                foreach (var infoLoggerInfo in info.LoggerInfos)
                 {
-                    Console.WriteLine ( infoLoggerInfo.TargetName ) ;
+                    Console.WriteLine(infoLoggerInfo.TargetName);
                 }
 
-                foreach ( var ci in info.ComponentInfos )
+                foreach (var ci in info.ComponentInfos)
                 {
-                    Console.WriteLine ( ci.Id ) ;
-                    foreach(var i in ci.Instances)
+                    Console.WriteLine(ci.Id);
+                    foreach (var i in ci.Instances)
                     {
-                        Console.WriteLine ( @"	" + i.Desc ) ;
+                        Console.WriteLine(@"	" + i.Desc);
                     }
                 }
-                
+
             }
         }
     }
 
     internal class Test1
     {
-        public Test2 Test2 { get ; set ; }
+        public Test2 Test2 { get; set; }
     }
 
     internal class Test2
     {
-        public string Hello { get ; set ; }
+        public string Hello { get; set; }
     }
 }
