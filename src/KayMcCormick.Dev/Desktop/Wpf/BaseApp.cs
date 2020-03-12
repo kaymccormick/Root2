868c74208d4e674c58be20f77c96d63a2c25ea98
GitTask <gittask@notused.com>
2020-03-12 03:01:09 -0700

    autofac

--- src/KayMcCormick.Dev/Desktop/Wpf/BaseApp.cs
+++ src/KayMcCormick.Dev/Desktop/Wpf/BaseApp.cs
@@ -120,7 +120,7 @@ namespace KayMcCormick.Lib.Wpf
         {
             foreach ( var module in GetModules ( ) )
             {
-                Logger.Debug ( "Adding module {module}" , module ) ;
+                Logger.Debug ( "Adding module {module}" , module .ToString()) ;
                 appInst.AddModule(module);
             }
             appInst.Initialize ( ) ;
