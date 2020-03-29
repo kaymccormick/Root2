5bdcb542395bfdc10566cfeb8b43d8066e869e34
GitTask <gittask@notused.com>
2020-03-29 13:17:48 -0700

    massive fixing

---
+++ src/KayMcCormick.Dev/Desktop/Analysis/AnalysisControls/JsonLazyConverterFactory.cs
@@ -0,0 +1,74 @@
+#region header
+// Kay McCormick (mccor)
+// 
+// Analysis
+// AnalysisControls
+// JsonLazyConverterFactory.cs
+// 
+// 2020-03-29-11:13 AM
+// 
+// ---
+#endregion
+using System ;
+using System.Text.Json ;
+using System.Text.Json.Serialization ;
+
+namespace AnalysisControls
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class JsonLazyConverterFactory : JsonConverterFactory
+    {
+        #region Overrides of JsonConverter
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="typeToConvert"></param>
+        /// <returns></returns>
+        public override bool CanConvert ( Type typeToConvert )
+        {
+            if ( typeToConvert.IsGenericType
+                 && typeToConvert.GetGenericTypeDefinition ( ) == typeof ( Lazy <> ) )
+            {
+                return true ;
+            }
+
+            return false ;
+        }
+        #endregion
+        #region Overrides of JsonConverterFactory
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="typeToConvert"></param>
+        /// <param name="options"></param>
+        /// <returns></returns>
+        public override JsonConverter CreateConverter (
+            Type                  typeToConvert
+          , JsonSerializerOptions options
+        )
+        {
+            var t = typeToConvert.GetGenericArguments ( )[ 0 ] ;
+            var ctype = typeof ( InnerConverter <,> ).MakeGenericType ( typeToConvert, t) ;
+            return ( JsonConverter ) Activator.CreateInstance ( ctype ) ;
+        }
+
+        private class InnerConverter<T, X> : JsonConverter <T> where T : Lazy<X>
+        {
+            #region Overrides of JsonConverter<T>
+            public override T Read ( ref Utf8JsonReader reader , Type typeToConvert , JsonSerializerOptions options ) { return default ; }
+
+            public override void Write (
+                Utf8JsonWriter        writer
+              , T                     value
+              , JsonSerializerOptions options
+            )
+            {
+                writer.WriteStringValue ( "Lazy" ) ;
+            }
+            #endregion
+        }
+        #endregion
+    }
+}
