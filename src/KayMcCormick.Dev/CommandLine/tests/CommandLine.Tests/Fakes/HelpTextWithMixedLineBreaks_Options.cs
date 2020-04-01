324902686cc7475deb2b2cf37a41a4b34c44ede0
GitTask <gittask@notused.com>
2020-04-01 14:19:49 -0700

    rm

--- src/KayMcCormick.Dev/CommandLine/tests/CommandLine.Tests/Fakes/HelpTextWithMixedLineBreaks_Options.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CommandLine.Tests.Fakes
-{
-    public class HelpTextWithMixedLineBreaks_Options
-    {
-        [Option(HelpText = 
-            "This is a help text description\n  It has multiple lines.\r\n  Third line")]
-        public string StringValue { get; set; }
-    }
-}
