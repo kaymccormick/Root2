2bf7b2005e72c4124ca26cc3c067a4f54db12edc
GitTask <gittask@notused.com>
2020-04-15 02:18:59 -0700

    broke it

---
+++ src/KayMcCormick.Dev/Desktop/Analysis/AnalysisAppLib/DocInterface.cs
@@ -0,0 +1,55 @@
+﻿#region header
+// Kay McCormick (mccor)
+// 
+// Analysis
+// AnalysisAppLib
+// DocInterface.cs
+// 
+// 2020-04-15-1:39 AM
+// 
+// ---
+#endregion
+using System ;
+using System.Collections.Generic ;
+using AnalysisAppLib.XmlDoc ;
+using JetBrains.Annotations ;
+using KayMcCormick.Dev ;
+
+namespace AnalysisAppLib
+{
+    /// <summary>
+    /// Implementation of <see cref="IDocInterface"/>
+    /// </summary>
+    public class DocInterface : IDocInterface
+    {
+
+        private readonly Dictionary<Type, TypeDocInfo>
+            _docs = new Dictionary<Type, TypeDocInfo>();
+
+        private DocumentCollection _documentCollection ;
+        #region Implementation of IDocInterface
+        /// <inheritdoc />
+        public TypeDocInfo GetTypeDocumentation ( Type type )
+        {
+            return _docs[ type ] ;
+        }
+
+        /// <inheritdoc />
+        public void CollectDoc([CanBeNull] CodeElementDocumentation docNode)
+        {
+            if (docNode == null)
+            {
+                return;
+            }
+
+            DebugUtils.WriteLine($"{docNode}");
+            DocumentCollection.Add(docNode);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public DocumentCollection DocumentCollection { get { return _documentCollection ; } set { _documentCollection = value ; } }
+        #endregion
+    }
+}
