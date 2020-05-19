7bb681abcb701514d49f396b06aab2e18bedd428
Kay McCormick <kay@kaymccormick.com>
2020-05-19 01:29:32 -0700

    update

---
+++ src/KayMcCormick.Dev/Desktop/Analysis/Client2/UserDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Client2
+{
+    public class UserDbContext : DbContext
+    {
+        //public DbSet<>
+    }
+}
